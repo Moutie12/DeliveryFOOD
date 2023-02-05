770c965675b5c5bc65f44d7dfb4948dfd465c0a1
Moutie <108123837+Moutie12@users.noreply.github.com>
2023-02-05 06:50:02 -0800

    Delete Backend directory

--- Backend/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
-$conn=mysqli_connect('localhost','root','','grud_angular');
-session_start() ;
-echo json_encode($_SESSION['username']);
-
-
-?>
